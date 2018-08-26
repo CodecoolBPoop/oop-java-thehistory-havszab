1b944bc2e463ecf92e281077b4759ba28f1dd380
havszab <havaldaszab@gmail.com>
2018-08-26 16:39:21 +0200

    Array class changes

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -11,38 +11,53 @@ public class TheHistoryArray implements TheHistory {
 
     @Override
     public void add(String text) {
-
+        String[] splittedStringArr = text.split(" ");
+        wordsArray = new String[splittedStringArr.length];
+        System.arraycopy(splittedStringArr, 0, wordsArray, 0, splittedStringArr.length);
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         for (int i = 0; i<wordsArray.length; i++) {
             if(wordsArray[i] == wordToBeRemoved)
-                wordsArray[i] = null;
+                //System.arraycopy(arr, removedIdx + 1, arr, removedIdx, arr.length - 1 - removedIdx);
+                System.arraycopy(wordsArray, i+1, wordsArray, i,wordsArray.length-1-i);
         }
     }
 
     @Override
     public int size() {
-        int arrSize = wordsArray.length;
-        return arrSize;
+        return wordsArray.length;
     }
 
     @Override
     public void clear() {
-
+        wordsArray = new String[0];
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
-        for(String word : wordsArray)
-            if (word == from)
-                word = to;
+        for (int i = 0; i <= wordsArray.length-1; i++) {
+            if (wordsArray[i].equals(from)) {
+                wordsArray[i] = to;
+            }
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
-        //TODO: check the TheHistory interface for more information
+        String[] arrPartToCompare = new String[fromWords.length];
+        for (int arrIdx = 0; arrIdx <= wordsArray.length - fromWords.length; arrIdx++) {
+            System.arraycopy(wordsArray, arrIdx, arrPartToCompare, 0, fromWords.length);
+            // Check equality of part of source array and array of words to be replaced
+            if(Arrays.equals(fromWords, arrPartToCompare)) {
+                //copy end of the Array
+                String[] arrEnd = new String[wordsArray.length-arrIdx+1+fromWords.length ];
+                System.arraycopy(wordsArray, arrIdx+1+fromWords.length, arrEnd, 0, arrEnd.length);
+                }
+                //System.out.println(Arrays.toString(wordsArray));for (int replaceIdx = 0; replaceIdx <= fromWords.length-1; replaceIdx++) {
+                //wordsArray[replaceIdx+arrIdx] = toWords[replaceIdx];
+        }
     }
 
     @Override
